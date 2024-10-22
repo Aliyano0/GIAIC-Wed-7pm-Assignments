9560f2d9757818dd722434f276e1d17ee6abbe95
Aliyano0 <aliyanaqeel97@gmail.com>
2024-10-23 04:33:45 +0500

    Forgot to add not-found and error page

---
+++ Wed-Assignment-1/src/app/error.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div className="w-full h-[80vh] flex items-center justify-center text-indigo-800">
+      <h1 className="text-5xl font-bold">ERROR WHILE LOADING THE PAGE</h1>
+    </div>
+  );
+}
---
+++ Wed-Assignment-1/src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="w-full h-[80vh] flex items-center justify-center text-indigo-800">
+      <h1 className="text-5xl font-bold">OOPS! PAGE NOT FOUND.</h1>
+    </div>
+  );
+}
